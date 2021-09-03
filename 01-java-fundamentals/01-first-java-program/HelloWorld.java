15ff551c88f0f2cf7842c822cb9dada2a6b845bb
HannahMulligan-hm <hannahx08@comcast.net>
2021-09-03 16:08:26 -0400

    Hello world/first program

---
+++ 01-java-fundamentals/01-first-java-program/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Hannah");
+		System.out.println("I am 19 years old");
+		System.out.println("My hometown is Warrington, PA");
+	}
+}
